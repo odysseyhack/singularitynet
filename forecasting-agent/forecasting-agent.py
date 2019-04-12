32eb348cbec4130cb1ba154fe4df38685ea5e3a0
Vitaly Bogdanov <vsbogd@gmail.com>
2019-04-12 22:05:15 +0300

    Using SingularityNET SDK to call a forecasting service

--- forecasting-agent/forecasting-agent.py
+++ forecasting-agent/forecasting-agent.py
@@ -4,6 +4,9 @@ import grpc
 import oracle_pb2
 import oracle_pb2_grpc
 
+from snet_cli.sdk import Session, Client, AutoFundingFundingStrategy
+from snet_cli.config import Config
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description = 'Forecasting agent use ' +
             'SingularityNET forecasting service to predict gas consumption ' +
@@ -11,10 +14,31 @@ def parse_arguments():
     parser.add_argument('--oracle_endpoint', help = 'Oracle endpoint')
     return parser.parse_args()
 
+def get_forecasting_producer():
+    session = Session(Config())
+    client = Client(session, "snet", "cntk-lstm-forecast", AutoFundingFundingStrategy(amount_cogs = 10, expiration = "+10days"))
+    return client
+
+def send_data(producer, consumer):
+    input = producer.classes.Input(
+                window_len = 20,
+                word_len = 5,
+                alphabet_size = 5,
+                source_type = 'financial',
+                source = 'yahoo',
+                contract = 'AMZN',
+                start_date = "2017-01-01",
+                end_date = "2018-12-10"
+            )
+    output = producer.stub.forecast(input)
+    print('output:', output)
+
 def run():
     args = parse_arguments()
     with grpc.insecure_channel(args.oracle_endpoint) as channel:
-        forecastingAPI = oracle_pb2_grpc.ForecasterStub(channel)
+        forecasting_consumer = oracle_pb2_grpc.ForecasterStub(channel)
+        forecasting_producer = get_forecasting_producer()
+        send_data(forecasting_producer, forecasting_consumer)
 
 if __name__ == '__main__':
     run()
