41ae0ffec45bcba6a32f2aab58624c2334055195
Vitaly Bogdanov <vsbogd@gmail.com>
2019-04-13 11:58:21 +0300

    Add option to set data pushing period in seconds

--- forecasting-agent/forecasting-agent.py
+++ forecasting-agent/forecasting-agent.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 import grpc
 import oracle_pb2
@@ -11,7 +12,9 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description = 'Forecasting agent use ' +
             'SingularityNET forecasting service to predict gas consumption ' +
             'and publish it via oracle service.')
-    parser.add_argument('--oracle_endpoint', help = 'Oracle endpoint')
+    parser.add_argument('--oracle-endpoint', help = 'Oracle endpoint')
+    parser.add_argument('--period-seconds', help = 'Time period to get data ' +
+            'from the service and push it to the oracle in seconds');
     return parser.parse_args()
 
 def get_forecasting_producer():
@@ -38,7 +41,10 @@ def run():
     with grpc.insecure_channel(args.oracle_endpoint) as channel:
         forecasting_consumer = oracle_pb2_grpc.ForecasterStub(channel)
         forecasting_producer = get_forecasting_producer()
-        send_data(forecasting_producer, forecasting_consumer)
+        while True:
+            send_data(forecasting_producer, forecasting_consumer)
+            time.sleep(args.period_seconds)
+
 
 if __name__ == '__main__':
     run()
