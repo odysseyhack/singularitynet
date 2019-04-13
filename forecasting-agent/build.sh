9053b5f27ae16b93b4691ae7fc25b3766696ee20
Vitaly Bogdanov <vsbogd@gmail.com>
2019-04-13 13:22:43 +0300

    Add SingularityNET SDK installation steps

--- forecasting-agent/build.sh
+++ forecasting-agent/build.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+
 ROOT=..
-python -m grpc_tools.protoc -I${ROOT}/oracle/protos --python_out=.  --grpc_python_out=. ${ROOT}/oracle/protos/oracle.proto
+
+# compile protobuf
+python -m grpc_tools.protoc -I${ROOT}/oracle/protos --python_out=.  \
+	--grpc_python_out=. ${ROOT}/oracle/protos/oracle.proto
+
+# install SingularityNET SDK
+mkdir -p vendor
+git clone --depth=1 --single-branch --branch new-sdk \
+	git@github.com:astroseger/snet-cli.git \
+	./vendor/snet-cli
+pushd vendor/snet-cli
+sudo apt-get install libudev-dev libusb-1.0-0-dev
+./scripts/blockchain install
+pip install -e .
+popd
