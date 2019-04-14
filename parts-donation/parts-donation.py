9ac64559bacdf5b65945f7c759f6001403cbc3cc
Vitaly Bogdanov <vsbogd@gmail.com>
2019-04-14 10:55:01 +0300

    Adding dockerfile to run OpenCog infrastructure

---
+++ parts-donation/parts-donation.py
@@ -0,0 +1,16 @@
+from opencog.atomspace import AtomSpace
+from opencog.utilities import initialize_opencog, finalize_opencog
+from opencog.type_constructors import *
+from opencog.bindlink import execute_atom
+import __main__
+
+def define_knowledge_base():
+    pass
+
+    
+def run():
+    define_knowledge_base()
+    print("Hello, world")
+
+if __name__ == '__main__':
+    run()
---
+++ parts-donation/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+PWD=`pwd`
+REPO=`dirname ${PWD}`
+
+docker run \
+	-v $REPO:/home/opencog/singularitynet \
+	-ti parts-donation \
+	python3 /home/opencog/singularitynet/parts-donation/parts-donation.py
