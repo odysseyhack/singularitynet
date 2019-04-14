9de3c2a4603aa6e6c3900576214b1aeef7363b4d
gontijo <arturgontijo@gmail.com>
2019-04-14 07:53:33 +0200

    [LSTM-Forecast] Removing Protobuf gen-codes (Better Code)

--- ai/fonio-forecast/service/time_series_forecast_service.py
+++ ai/fonio-forecast/service/time_series_forecast_service.py
@@ -27,15 +27,8 @@ GPU_QUEUE_ID = -1
 # derived from the protobuf codes.
 class ForecastServicer(grpc_bt_grpc.ForecastServicer):
     def __init__(self):
-        # LSTM pre-trained Model
-        self.model_file = ""
         self.csv_file = "fonio.csv"
-        self.input_dim = 0
-        
-        # Number of points to forecast
-        self.num_points = 0
-        
-        self.response = ""
+        self.response = Output()
         log.debug("ForecastServicer created")
 
     # The method that will be exposed to the snet-cli call command.
@@ -45,45 +38,17 @@ class ForecastServicer(grpc_bt_grpc.ForecastServicer):
         gpu_queue_id = get_gpu_queue_id()
         try:
             # Wait to use GPU (max: 1h)
-            count = 0
-            while GPU_DEVICE_BUSY or GPU_QUEUE[0] != gpu_queue_id:
-                time.sleep(1)
-                if count % 60 == 0:
-                    log.debug("[Client: {}] GPU is being used by [{}], waiting...".format(gpu_queue_id, GPU_QUEUE[0]))
-                count += 1
-                if count > 60 * 60:
-                    self.response = Output()
-                    self.response.output_points = "GPU Busy"
-                    return self.response
+            if wait_for_gpu(gpu_queue_id):
+                return self.response
 
             # Lock GPU usage
             acquire_gpu(gpu_queue_id)
 
-            # In our case, request is a Input() object (from .proto file)
-            self.model_file = request.model_file
-            self.num_points = request.num_points
-            self.input_dim = request.input_dim
-            
-            # To respond we need to create a Output() object (from .proto file)
-            self.response = Output()
+            fc = Forecast(request.model_file, self.csv_file, request.input_dim)
+            y_data, predictions = fc.forecast(request.num_points)
+            self.response = Output(output_points=predictions)
 
-            fc = Forecast(self.model_file,
-                          self.csv_file,
-                          self.input_dim)
-
-            y_data, predictions = fc.forecast(self.num_points)
-            # plot_predictions(y_data, predictions)
-
-            response = {"points": []}
-            for idx, point in enumerate(predictions):
-                response["points"].append(point)
-
-            self.response.output_points = response["points"]
-
-            log.debug("forecast({},{}) = {}".format(self.model_file,
-                                                    self.num_points,
-                                                    self.response.output_points))
-
+            log.debug("forecast({},{}) = {}".format(request.model_file, request.num_points, self.response.output_points))
             # Unlock GPU usage
             release_gpu(gpu_queue_id)
             return self.response
@@ -94,8 +59,6 @@ class ForecastServicer(grpc_bt_grpc.ForecastServicer):
                 release_gpu(gpu_queue_id)
             else:
                 remove_from_queue(gpu_queue_id)
-            self.response = Output()
-            self.response.output_points = "Fail"
             return self.response
 
 
@@ -106,6 +69,19 @@ def get_gpu_queue_id():
     GPU_QUEUE.append(GPU_QUEUE_ID)
     log.debug("[Client: {}]                  GPU_QUEUE     : {}".format(GPU_QUEUE_ID, GPU_QUEUE))
     return GPU_QUEUE_ID
+
+
+def wait_for_gpu(gpu_queue_id):
+    count = 0
+    while GPU_DEVICE_BUSY or GPU_QUEUE[0] != gpu_queue_id:
+        time.sleep(1)
+        if count % 60 == 0:
+            log.debug("[Client: {}] GPU is being used by [{}], waiting...".format(gpu_queue_id, GPU_QUEUE[0]))
+        count += 1
+        if count > 60 * 60:
+            log.error("GPU Busy")
+            return True
+    return False
 
 
 def remove_from_queue(gpu_queue_id):
